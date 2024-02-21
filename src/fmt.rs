606a2785bfc824caf6c365666a488f656d321516
dimak <rahbussiness@gmail.com>
2024-02-21 20:20:14 +0100

    Add: Display for ShortUuid

---
+++ src/fmt.rs
@@ -0,0 +1,9 @@
+use core::fmt;
+
+use crate::ShortUuid;
+
+impl fmt::Display for ShortUuid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        String::from_utf8(self.0.to_vec()).unwrap().fmt(f)
+    }
+}
