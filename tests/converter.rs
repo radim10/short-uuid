897799cb516274bd121383584778f6132a8e2ed7
dimak <rahbussiness@gmail.com>
2024-02-22 17:57:37 +0100

    Update: remove constant

--- tests/converter.rs
+++ tests/converter.rs
@@ -2,10 +2,6 @@
 mod tests {
     use short_uuid::converter::BaseConverter;
 
-    const FLICKR_BASE_58: &str = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-    // const COOKIE_BASE_90: &str =
-    //     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
-
     #[test]
     fn test_flickr_base_conversion() {
         let uuid_string = "0408510d-ce4f-4761-ab67-2dfe2931c898";
