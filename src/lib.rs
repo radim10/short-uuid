7c79068389a03c4e9dafdccab0420fd102b39873
dimak <rahbussiness@gmail.com>
2024-02-22 14:46:03 +0100

    Update: ShortUuid methods results

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use converter::BaseConverter;
+use error::ErrorKind;
 
 pub mod converter;
+mod error;
 mod fmt;
 
 pub const FLICKR_BASE: &str = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
 
 pub const COOKIE_BASE_90: &str =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
+
+type UuidError = uuid::Error;
 
 // pub type Bytes = [u8; 16];
 pub type Bytes = Vec<u8>;
@@ -23,7 +27,7 @@ impl ShortUuid {
         // clean string
         let cleaned = uuid_string.to_lowercase().replace("-", "");
 
-        let converter = BaseConverter::new(FLICKR_BASE);
+        let converter = BaseConverter::new(FLICKR_BASE).unwrap();
 
         // convert to selected base
         let result = converter.convert(&cleaned);
@@ -32,33 +36,46 @@ impl ShortUuid {
     }
 
     /// Convert uuid to short format using flickrBase58
-    pub fn from_uuid(uuid_string: &str) -> ShortUuid {
+    pub fn from_uuid(uuid_string: &str) -> Result<ShortUuid, UuidError> {
+        // validate
+        uuid::Uuid::parse_str(uuid_string)?;
+
         let cleaned = uuid_string.to_lowercase().replace("-", "");
 
-        let converter = BaseConverter::new(FLICKR_BASE);
+        let converter = BaseConverter::new(FLICKR_BASE).unwrap();
 
         // convert to selected base
         let result = converter.convert(&cleaned);
 
-        ShortUuid(result)
+        Ok(ShortUuid(result))
     }
 
     /// Convert uuid to short format using flickrBase58
-    pub fn from_uuid_custom(uuid_string: &str, custom_base: &'static str) -> ShortUuid {
-        let cleaned = uuid_string.to_lowercase().replace("-", "");
+    pub fn from_uuid_custom(
+        uuid_string: &str,
+        custom_base: &'static str,
+    ) -> Result<ShortUuid, ErrorKind> {
+        // validate
+        let is_valid_uuid = uuid::Uuid::parse_str(uuid_string);
 
-        let converter = BaseConverter::new(custom_base);
+        if let Err(e) = is_valid_uuid {
+            return Err(ErrorKind::UuidError(e));
+        }
+
+        let converter = BaseConverter::new(custom_base).map_err(|_| ErrorKind::EmptyAlphabet)?;
+
+        let cleaned = uuid_string.to_lowercase().replace("-", "");
 
         // convert to selected base
         let result = converter.convert(&cleaned);
 
-        ShortUuid(result)
+        Ok(ShortUuid(result))
     }
 
     /// Convert short to uuid
     pub fn to_uuid(self) -> uuid::Uuid {
         // Convert to hex
-        let to_hex_converter = BaseConverter::new(FLICKR_BASE);
+        let to_hex_converter = BaseConverter::new(FLICKR_BASE).unwrap();
 
         // Convert to hex string
         let result = to_hex_converter.convert_to_hex(&self.0).unwrap();
@@ -68,14 +85,17 @@ impl ShortUuid {
     }
 
     /// Convert short to uuid using custom base
-    pub fn to_uuid_custom(self, custom_base: &'static str) -> uuid::Uuid {
-        let to_hex_converter = BaseConverter::new(custom_base);
+    pub fn to_uuid_custom(self, custom_base: &'static str) -> Result<uuid::Uuid, ErrorKind> {
+        let to_hex_converter =
+            BaseConverter::new(custom_base).map_err(|_| ErrorKind::EmptyAlphabet)?;
 
         // Convert to hex string
         let result = to_hex_converter.convert_to_hex(&self.0).unwrap();
 
         // Format hex string as uuid
-        format_uuid(result)
+        let uuid_value = format_uuid(result);
+
+        Ok(uuid_value)
     }
 
     pub fn as_slice(&self) -> &[u8] {
