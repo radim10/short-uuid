cf132012e8a0e7dd493d579f9f8c2af45e13f3a3
dimak <rahbussiness@gmail.com>
2024-02-22 11:40:11 +0100

    Add: to_uuid method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use converter::BaseConverter;
+
 pub mod converter;
 mod fmt;
 
@@ -8,6 +10,8 @@ const COOKIE_BASE_90: &str =
 
 // pub type Bytes = [u8; 16];
 pub type Bytes = Vec<u8>;
+
+#[derive(Debug)]
 pub struct ShortUuid(Bytes);
 
 impl ShortUuid {
@@ -19,7 +23,7 @@ impl ShortUuid {
         // clean string
         let cleaned = uuid_string.to_lowercase().replace("-", "");
 
-        let converter = converter::BaseConverter::new(FLICKR_BASE);
+        let converter = BaseConverter::new(FLICKR_BASE);
 
         // convert to selected base
         let result = converter.convert(&cleaned);
@@ -31,12 +35,35 @@ impl ShortUuid {
     pub fn from_uuid(uuid_string: &str) -> ShortUuid {
         let cleaned = uuid_string.to_lowercase().replace("-", "");
 
-        let converter = converter::BaseConverter::new(FLICKR_BASE);
+        let converter = BaseConverter::new(FLICKR_BASE);
 
         // convert to selected base
         let result = converter.convert(&cleaned);
 
         ShortUuid(result)
+    }
+
+    /// Convert short to uuid
+    pub fn to_uuid(self) -> uuid::Uuid {
+        // Convert to hex
+        let to_hex_converter = BaseConverter::new(FLICKR_BASE);
+
+        let result = to_hex_converter.convert_to_hex(&self.0).unwrap();
+        dbg!(&result);
+
+        let formatted_uuid = format!(
+            "{}-{}-{}-{}-{}",
+            &result[0..8],
+            &result[8..12],
+            &result[12..16],
+            &result[16..20],
+            &result[20..32]
+        );
+
+        // Should not fail
+        let uuid = uuid::Uuid::parse_str(&formatted_uuid).unwrap();
+
+        return uuid;
     }
 
     pub fn as_slice(&self) -> &[u8] {
