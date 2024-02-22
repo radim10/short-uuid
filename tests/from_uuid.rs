a255f091fb7c9212ef39daabc8373df55f783cf7
dimak <rahbussiness@gmail.com>
2024-02-22 17:56:28 +0100

    Update: test name

--- tests/from_uuid.rs
+++ tests/from_uuid.rs
@@ -3,7 +3,7 @@ mod tests {
     use short_uuid::ShortUuid;
 
     #[test]
-    fn generate_short_uuid() {
+    fn from_uuid_str() {
         let generated = ShortUuid::from_uuid_str("4308510d-ce4f-4761-ab67-2dfe2931c834");
 
         match generated {
