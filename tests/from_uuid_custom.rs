a630f89233ed0011fa2f490e9de1246779b3f111
dimak <rahbussiness@gmail.com>
2024-02-22 17:56:51 +0100

    Update: test custom_alphabet

--- tests/from_uuid_custom.rs
+++ tests/from_uuid_custom.rs
@@ -3,17 +3,15 @@ mod tests {
     use short_uuid::ShortUuid;
 
     #[test]
-    fn short_custom_base() {
-        let custom_base = "sb";
+    fn from_uuid_str_custom() {
+        let custom_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
         let uuid_string = "0408510d-ce4f-4761-ab67-2dfe2931c893";
 
-        let generated = ShortUuid::from_uuid_str_custom(&uuid_string, custom_base);
+        let generated = ShortUuid::from_uuid_str_custom(&uuid_string, custom_alphabet);
 
         match generated {
             Ok(generated) => {
-                dbg!(&generated.to_string());
-
-                let uuid = ShortUuid::to_uuid_custom(generated, custom_base).unwrap();
+                let uuid = ShortUuid::to_uuid_custom(generated, custom_alphabet).unwrap();
                 dbg!(&uuid);
                 assert_eq!(uuid.to_string(), uuid_string);
             }
