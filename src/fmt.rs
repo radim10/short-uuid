5b8cd7fe81b27021f75c9cf920a034a7920f8d6e
dimak <rahbussiness@gmail.com>
2024-02-22 21:42:22 +0100

    Add: Display for ShortUuidCustom

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,8 +1,14 @@
 use std::fmt;
 
-use crate::ShortUuid;
+use crate::{ShortUuid, ShortUuidCustom};
 
 impl fmt::Display for ShortUuid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        String::from_utf8(self.0.to_vec()).unwrap().fmt(f)
+    }
+}
+
+impl fmt::Display for ShortUuidCustom {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         String::from_utf8(self.0.to_vec()).unwrap().fmt(f)
     }
