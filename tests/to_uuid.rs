7ee8e55a252c69b8d25df3dbf1ed2140f953fd29
dimak <rahbussiness@gmail.com>
2024-02-22 11:39:09 +0100

    Add: Test convert from uuid and back

---
+++ tests/to_uuid.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod tests {
+    use short_uuid::ShortUuid;
+
+    #[test]
+    fn convert_from_uuid_and_back() {
+        let uuid = uuid::Uuid::new_v4();
+        let uuid_string = uuid.to_string();
+
+        let generated = ShortUuid::from_uuid(&uuid_string);
+        let _ = generated.to_string();
+
+        dbg!("{:#?}", &generated);
+        dbg!(&generated.to_string());
+
+        let uuid = ShortUuid::to_uuid(generated);
+
+        assert_eq!(uuid.to_string(), uuid_string);
+        dbg!(&uuid);
+    }
+}
