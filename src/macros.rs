74be2f82565a441b65e58ed5f3ff6e9366bb20fa
dimak <rahbussiness@gmail.com>
2024-02-23 14:45:51 +0100

    Update: macro function + doc comment

--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,12 @@
-/// Generate a short UUID v4 in flickrBase58
+/// Generate a short UUID v4 in flickrBase58 alphabet
+/// ## Usage
+/// ```rust
+/// use short_uuid::short;
+/// let generated = short!();
+/// ```
 #[macro_export]
 macro_rules! short {
     () => {
-        generate_short()
+        $crate::ShortUuid::generate()
     };
 }
