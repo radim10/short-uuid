30536e6c7d63260a29e3d2e144d975afa55b2e3a
dimak <rahbussiness@gmail.com>
2024-02-23 13:55:51 +0100

    Add: error test case

--- tests/from_uuid.rs
+++ tests/from_uuid.rs
@@ -3,19 +3,18 @@ mod tests {
     use short_uuid::ShortUuid;
 
     #[test]
-    fn from_uuid_str() {
-        let generated = ShortUuid::from_uuid_str("4308510d-ce4f-4761-ab67-2dfe2931c834");
+    fn from_uuid_str_success() {
+        let generated = ShortUuid::from_uuid_str("4308510d-ce4f-4761-ab67-2dfe2931c834")
+            .expect("Failed to generate short uuid");
 
-        match generated {
-            Ok(generated) => {
-                let generated_string = generated.to_string();
-                dbg!(&generated_string);
+        let generated_string = generated.to_string();
+        assert_eq!(generated_string.len(), 22);
+    }
 
-                assert_eq!(generated_string.len(), 22);
-            }
-            Err(err) => {
-                eprintln!("{:?}", err);
-            }
-        }
+    #[test]
+    fn from_uuid_str_errors() {
+        let generated = ShortUuid::from_uuid_str("").expect_err("Test should have failed");
+
+        dbg!(&generated);
     }
 }
