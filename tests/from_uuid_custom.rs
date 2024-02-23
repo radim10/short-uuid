d884c67db4d54c994e9aefffe0ae4208db6216a0
dimak <rahbussiness@gmail.com>
2024-02-23 12:56:42 +0100

    Update: Use custom translator

--- tests/from_uuid_custom.rs
+++ tests/from_uuid_custom.rs
@@ -1,17 +1,19 @@
 #[cfg(test)]
 mod tests {
-    use short_uuid::ShortUuidCustom;
+    use short_uuid::{CustomTranslator, ShortUuidCustom};
 
     #[test]
     fn from_uuid_str_custom() {
         let custom_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
         let uuid_string = "0408510d-ce4f-4761-ab67-2dfe2931c893";
 
-        let generated = ShortUuidCustom::from_uuid_str(&uuid_string, custom_alphabet);
+        let translator = CustomTranslator::new(custom_alphabet).unwrap();
+
+        let generated = ShortUuidCustom::from_uuid_str(&uuid_string, &translator);
 
         match generated {
             Ok(generated) => {
-                let uuid = ShortUuidCustom::to_uuid(generated, custom_alphabet).unwrap();
+                let uuid = generated.to_uuid(&translator).unwrap();
 
                 dbg!(&uuid);
                 assert_eq!(uuid.to_string(), uuid_string);
--- tests/parse_str_custom.rs
+++ tests/parse_str_custom.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use short_uuid::{ShortUuid, ShortUuidCustom};
+    use short_uuid::{CustomTranslator, ShortUuidCustom};
 
     #[test]
     fn from_uuid_str_custom() {
@@ -8,7 +8,10 @@ mod tests {
         let custom_alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk/";
         let uuid_string = "0408510d-ce4f-4761-ab67-2dfe2931c893";
 
-        let generated = ShortUuidCustom::from_uuid_str(&uuid_string, custom_alphabet);
+        let translator = CustomTranslator::new(custom_alphabet).unwrap();
+        let translator2 = CustomTranslator::new(custom_alphabet2).unwrap();
+
+        let generated = ShortUuidCustom::from_uuid_str(&uuid_string, &translator);
 
         match generated {
             Ok(generated) => {
@@ -17,11 +20,10 @@ mod tests {
                 dbg!(&short_string);
 
                 //
-                let from_short =
-                    ShortUuidCustom::parse_str(&short_string, custom_alphabet).unwrap();
+                let from_short = ShortUuidCustom::parse_str(&short_string, &translator2).unwrap();
                 dbg!(&from_short);
 
-                let from_short_uuid = from_short.to_uuid(custom_alphabet2).unwrap();
+                let from_short_uuid = from_short.to_uuid(&translator).unwrap();
                 assert_eq!(from_short_uuid.to_string(), uuid_string);
             }
             Err(e) => eprintln!("{:?}", e.to_string()),
