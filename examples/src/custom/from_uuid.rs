990f8d38bd8c8d90f7117fc403bf5983da2faecc
dimak <rahbussiness@gmail.com>
2024-02-23 15:43:57 +0100

    Update: move uuid

--- examples/src/custom/from_uuid.rs
+++ examples/src/custom/from_uuid.rs
@@ -5,11 +5,11 @@ use short_uuid::CustomTranslator;
 
 fn main() {
     use short_uuid::ShortUuidCustom;
-    let uuid = uuid::Uuid::new_v4();
 
     let custom_alphabet = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
     let translator = CustomTranslator::new(custom_alphabet).unwrap();
 
+    let uuid = uuid::Uuid::new_v4();
     let short_custom = ShortUuidCustom::from_uuid(&uuid, &translator);
     let short_custom_string = short_custom.to_string();
 
